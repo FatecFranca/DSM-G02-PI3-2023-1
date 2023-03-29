91303c8ce3ac7e2c3fba8d380526d5bfa0ceafc5
gabriel.carrijo <gabriel.carrijo@perinity.com>
2023-03-28 21:54:15 -0300

    feat: criacao inicial do projeto

---
+++ fullstack/web/src/main/java/io/piterceiro/web/User.java
@@ -0,0 +1,10 @@
+package io.piterceito.web;
+
+public class User {
+
+    private String id;
+    private String email;
+    private String senha;
+    private String nome;
+
+}
---
+++ fullstack/web/src/test/java/io/piterceiro/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.piterceiro.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
