7252a545c0b0f9efe06e8ad82231011987d1129c
GabrielFCarrijo <carrijogit2003@gmail.com>
2023-05-30 20:33:27 -0300

    feat: criacao do novo endipoint de usuario/abastecimento

--- src/controllers/abastecimento.js
+++ src/controllers/abastecimento.js
@@ -3,42 +3,53 @@ const controller = {}
 
 controller.create = async (req, res) => {
     try {
-        const abastecimento = await Abastecimento.create(req.body);
-        res.status(200).json(abastecimento);
-    }
-    catch(error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-}
-
-controller.retrieveAll = async (req, res) => {
-    try {
-        const abastecimento = await Abastecimento.find({})
-        res.status(200).json(abastecimento)
+      const abastecimento = await Abastecimento.create(req.body);
+      res.status(200).json(abastecimento);
     } catch (error) {
-        res.status(500).json({message: error.message})
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
     }
-}
-
-controller.retrieveOne = async(req, res) => {
+  };
+  
+  controller.retrieveAll = async (req, res) => {
     try {
-        const {id} = req.params
-        const abastecimento = await Abastecimento.findById(id)
-        res.status(200).json(abastecimento)
+      const abastecimentos = await Abastecimento.find({});
+      const abastecimentoList = abastecimentos.map((abastecimento) => abastecimento.abastecimento);
+      res.status(200).json(abastecimentoList);
     } catch (error) {
-        res.status(500).json({message: error.message})
+      res.status(500).json({ message: error.message });
     }
-}
-
-controller.deleteAll = async (req, res) => {
+  };
+  
+  controller.retrieveOne = async (req, res) => {
     try {
-        await Abastecimento.deleteMany({}) // Exclui todos os documentos da coleção "Abastecimento"
-
-        res.status(200).json({ message: "Todos os abastecimentos foram excluídos com sucesso" })
+      const { id } = req.params;
+      const abastecimento = await Abastecimento.findById(id);
+      res.status(200).json(abastecimento.abastecimento);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+      res.status(500).json({ message: error.message });
     }
-}
+  };
+
+  controller.retrieveByCliente = async (req, res) => {
+    try {
+      const { clienteId } = req.params;
+      const abastecimentos = await Abastecimento.find({ clienteId });
+      res.status(200).json(abastecimentos);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+  controller.deleteAll = async (req, res) => {
+    try {
+      await Abastecimento.deleteMany({});
+  
+      res.status(200).json({ message: "Todos os abastecimentos foram excluídos com sucesso" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
 
 module.exports = controller
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const router  = express.Router()
-const controller = require('../controllers/usuario')
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/usuario');
+const abastecimentoController = require('../controllers/abastecimento');
 
-router.post('/', controller.create)
-router.get('/', controller.retrieveAll)
-router.delete('/', controller.deleteAll)
-router.get('/:id', controller.retrieveOne)
-router.put('/:id', controller.update)
-router.delete('/:id', controller.delete)
+router.get('/abastecimento/:id', abastecimentoController.retrieveByCliente);
+router.post('/', controller.create);
+router.get('/', controller.retrieveAll);
+router.delete('/', controller.deleteAll);
+router.get('/:id', controller.retrieveOne);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
 
-module.exports = router
+module.exports = router;
